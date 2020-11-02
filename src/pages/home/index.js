906c24afb40296381a343ceb8527a95d349b5079
li1195382526 <1195382526@qq.com>
2020-11-02 18:20:01 +0800

    增加轮播图

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,7 @@
 import Taro, { Component } from '@tarojs/taro';
 import { View, Swiper, SwiperItem } from '@tarojs/components';
 import { connect } from '@tarojs/redux';
-import { AtActionSheet, AtMessage, AtButton, AtTabs, AtTabsPane, AtActionSheetItem, AtTabBar, AtModal } from 'taro-ui'
+import { AtTabBar } from 'taro-ui'
 import Questionaires from '../../components/questionaire'
 import './index.scss';
 import List from '../../components/list';
@@ -57,14 +57,33 @@ class Home extends Component {
   render() {
     return (
       <View className='page'>
-        
+         <Swiper
+           className='test-h'
+           indicatorColor='#999'
+           indicatorActiveColor='#333'
+           vertical={false}
+           circular
+           indicatorDots
+           autoplay
+          >
+            <SwiperItem>
+                <Image src='../../assets/images/f1.jpg' style='width: 100%; height: 100%'></Image>
+            </SwiperItem>
+            <SwiperItem>
+                <Image src='../../assets/images/f2.jpg' style='width: 100%; height: 100%'></Image>
+            </SwiperItem>
+            <SwiperItem>
+                <Image src='../../assets/images/f3.jpg' style='width: 100%; height: 100%'></Image>
+            </SwiperItem>
+          </Swiper>
         {/* 底部bar */}
         <AtTabBar
           fixed
           tabList={[
             { title: '首页', iconType: 'home' },
-            { title: '模板库', iconType: 'list' },
-            { title: '个人中心', iconType: 'user' }
+            { title: '购物车', iconType: 'shopping-cart' },
+            { title: '分类', iconType: 'list' },
+            { title: '我的', iconType: 'user' }
           ]}
           onClick={this.handleClickBar}
           current={this.state.currentBar}
